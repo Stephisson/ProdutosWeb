223102dec509a4c4a9991707fc2fa03a9af5f772
Stephisson Galvao <stephissongalvao@fiotec.fiocruz.br>
2026-07-27 20:45:37 -0300

    Criação de componentes, navegação em rotas e criação das páginas de cadastro e consulta de produtos

---
+++ src/app/cadastrar-produtos/cadastrar-produtos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-produtos',
+  imports: [],
+  templateUrl: './cadastrar-produtos.html',
+  styleUrl: './cadastrar-produtos.css',
+})
+export class CadastrarProdutos {}
